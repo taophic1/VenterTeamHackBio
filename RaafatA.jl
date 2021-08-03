d4fa1c9cb6c61d66737591a16cabefee8a1c4bd7
Raafat Abdulmajeed Fahmy <76243557+RaafatA@users.noreply.github.com>
2021-08-03 13:07:03 +0200

    Add files via upload

---
+++ RaafatA.jl
@@ -0,0 +1,6 @@
+data = Dict("Name"=> "Raafat Abdelmajeed", 
+      "Email"=>"Raafat.abdulmajeed@gmail.com", 
+      "username"=>"@RaafatA")
+for (key, value) in data 
+    println(key ," : ", value)
+end 

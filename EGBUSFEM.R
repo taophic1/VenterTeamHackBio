06d6835c312ac5a0610685e47f9355d28eade84d
Egbusfem <85350295+Egbusfem@users.noreply.github.com>
2021-08-03 14:49:45 +0200

    Add files via upload

---
+++ EGBUSFEM.R
@@ -0,0 +1,8 @@
+myname="Samuel Oluwafemi Egbuwalo"
+myemail="femiegbuwalo@gmail.com"
+myusername="@Egbusfem"
+mybiostack="Drug Development and Genomics"
+print(myname)
+print(myusername)
+print(mybiostack)
+print(myemail)

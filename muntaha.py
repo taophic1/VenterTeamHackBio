115cb32c77bb0d41056d6b5e8913ed844d7bb8ed
muntahaUmer <88343721+muntahaUmer@users.noreply.github.com>
2021-08-04 06:12:53 -0700

    Update muntaha.py

--- muntaha.py
+++ muntaha.py
@@ -1,4 +1,29 @@
+def hamming_distance(myusername, mytwitter_handle):
+    # Start with a distance of zero, and count up
+    distance = 0
+    # Loop over the indices of the string
+    L = len(myusername)
+    for i in range(L):
+        # Add 1 to the distance if these two characters are not equal
+        if myusername[i] != mytwitter_handle[i]:
+            distance += 1
+    # Return the final count of differences
+    return distance
+
+
+
+
+
+
+
+
 def personal_details():
-myname, myemail,myusername,mybiostack = "muntaha","muntaha.bsbi589@iiu.edu.pk","muntahaUmer","drug developement anf genomics"
-print("myname: {}\nmyemail: {}\nmyusername: {}\nmybiostack: {}".format(myname, myemail, myusername,mybiostack))
+ name, email,slackusername,biostack,twitter_handle, = "muntaha","muntaha.bsbi589@iiu.pk","@muntaha","drug developement anf genomics","@muntaha111"
+
+ example_dist = hamming_distance("@muntaha", "@muntaha111")
+
+ print(example_dist)
+ print("name: {}\nemail: {}\nslack_username: {}\nbiostack: {}\ntwitter_handle:{}\nhamming_distance:{}".format(name, email, slackusername,biostack,twitter_handle,example_dist))
+
+
 personal_details()

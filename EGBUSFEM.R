1acd0fc2ef2e6163d50fb5a0aa2c28c91ff3cf11
Egbusfem <85350295+Egbusfem@users.noreply.github.com>
2021-08-04 14:02:52 +0200

    Delete EGBUSFEM.R

--- EGBUSFEM.R
+++
@@ -1,8 +0,0 @@
-myname="Samuel Oluwafemi Egbuwalo"
-myemail="femiegbuwalo@gmail.com"
-myusername="@Egbusfem"
-mybiostack="Drug Development and Genomics"
-print(myname)
-print(myusername)
-print(mybiostack)
-print(myemail)

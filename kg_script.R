7f102b3eae515ec0a14ff09b8ae68e4d31fa0f05
kg31blz <88341377+kg31blz@users.noreply.github.com>
2021-08-05 00:13:17 +0530

    Create kg_script.R

---
+++ kg_script.R
@@ -0,0 +1,11 @@
+{r}
+a = "kshitij gaikwad"
+b = "kshitijeaoff@gmail.com"
+c = "@kg"
+d = "drug development and discovery"
+e = "@kshitij31"
+cat(a,b,c,d,e)
+print("hamming distance")
+slack = c(c)
+twit = c(e)
+sum(slack != twit)

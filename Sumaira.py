f7ffec6c11bca6925f034a912faf0443682043e4
Sumaira0 <88341148+Sumaira0@users.noreply.github.com>
2021-08-04 19:58:02 +0500

    Create Sumaira.py

---
+++ Sumaira.py
@@ -0,0 +1,12 @@
+myname="Sumaira Fatima"
+myemail="sumaira.bsbi598@gmail.com"
+myusername="Sumaira"
+mybiostack="Drug development and Genomics"
+mytwitter="@sumaira"
+hammingdistance=1
+print(myname)
+print(myemail)
+print(myusername)
+print(mybiostack)
+print(mytwitter)
+print(hammingdistance)

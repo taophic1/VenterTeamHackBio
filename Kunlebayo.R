1f7b02cabde3f6521e9d005c9adcdf16820b29a4
Adebayo Adekunle <88288059+Kunlebayo@users.noreply.github.com>
2021-08-04 13:18:35 +0800

    Add files via upload

---
+++ Kunlebayo.R
@@ -0,0 +1,8 @@
+myname="Adebayo Oluwafemi Adekunle"
+myemail="adekunleadebayoolufemi@gmail.com"
+myusername="@Kunlebayo"
+mybiostack="drug development and genomics"
+print(myname)
+print(myemail)
+print(myusername)
+print(mybiostack)

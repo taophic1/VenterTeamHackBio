eb845a39bd6c0f1709fe9ecb5f1c9d777775bb23
Raafat Abdulmajeed Fahmy <76243557+RaafatA@users.noreply.github.com>
2021-08-04 23:26:58 +0200

    Update RaafatA_script.jl

--- RaafatA_script.jl
+++ RaafatA_script.jl
@@ -1,7 +1,21 @@
+function hamming_distance(username::String, twitterusername::String)
+    dist_counter= 0
+    for i in length(username)
+        if username[i] != twitterusername[i]
+            dist_counter+=1
+        end
+    end
+    return dist_counter
+end
+
+hd = hamming_distance("RaafatA", "RaafatE")
+
+
 data = Dict("Name"=> "Raafat Abdelmajeed", 
       "Email"=>"Raafat.abdulmajeed@gmail.com", 
       "username"=>"@RaafatA", 
-       "Biostack"=>"Drug development")
+       "Biostack"=>"Drug development",
+       "hamming distance"=> hd)
 for (key, value) in data 
     println(key ," : ", value)
 end
